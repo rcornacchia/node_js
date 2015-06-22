5d89ef3314fff3c7129d16f9592b488bcbb061ce
Rob Cornacchia <bobbycornacchia@gmail.com>
2015-06-21 22:58:40 -0400

    adding user functionality

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,47 @@ router.get('/helloworld', function(req, res) {
 	res.render('helloworld', { title: 'Hello, World!' });
 });
 
+/* GET Userlist page */
+router.get('/userlist', function(req, res) {
+	var db = req.db;
+	var collection = db.get('usercollection');
+	collection.find({}, {}, function(e, docs){
+		res.render('userlist', {
+			"userlist" : docs
+		});
+	});
+});
+
+/* GET New User Page */
+router.get('/newuser', function(req, res) {
+	res.render('newuser', { title: 'Add New User' });
+})
+
+/* POST to add User Service */
+router.post('/adduser', function(req, res) {
+	// Set our internal DB variable
+	var db = req.db;
+
+	// Get our form values
+	var userName = req.body.username;
+	var userEmail = req.body.useremail;
+
+	// Set our collection
+	var collection = db.get('usercollection');
+
+	// Submit to the db
+	collection.insert({
+		"username" : userName,
+		"email" : userEmail
+	}, function(err, doc) {
+		if (err) {
+			// if it failed, return error
+			res.send("There was a problem adding the information to the database.");
+		} else {
+			// otherwise forward to succes page
+			res.redirect("userlist");
+		}
+	});
+});
+
 module.exports = router;
